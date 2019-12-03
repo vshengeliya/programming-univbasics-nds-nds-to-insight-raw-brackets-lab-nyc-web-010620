7d46c2936e4cde5f32eef04691281174b2aea141
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-03 03:40:55 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,22 +1,31 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
-def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
+def nds=
+      {
+        [:name =>"Stephen Spielberg", :budget => "1357566430"],
+        [:name =>"Russo Brothers", :budget => '2281002470'],
+        [:name =>"James Cameron", :budget =>'2571020373'],
+        [:name =>"Spike Lee", :budget =>'256624217'],
+        [:name =>"Wachowski Siblings",:budget =>'806180282'],
+        [:name =>"Robert Zemeckis",:budget =>'1273838385'],
+        [:name =>"Quentin Tarantino",:budget =>'662738268]',
+        [:name =>"Martin Scorsese",:budget =>'636812242'],
+        [:name =>"Francis Ford Coppola",:budget => '509719258']
+      }
+      
+      grand_total = 0 
+      row_index = 0 
+      while row_index< nds.lenght do
+        column_index = 0 
+        while column_index< nds[row_index].lenght do
+        grand_total += nds[row_index][column_index][:budget]
+        column_index+=1 
+      end
+      row_index +=1 
+      
+    end
+    p grand_total
+
   nil
 end
